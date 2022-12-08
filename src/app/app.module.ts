de3d7a79d073bd50900f7d8559895717ed1a0e0d
Antoine PRONNIER (Funix) <antoine.pronnier@gmail.com>
2022-12-08 07:56:51 +0100

    Created modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { SidebarPacifistaComponent } from './components/sidebar/sidebar-pacifista/sidebar-pacifista.component';
-import { SidebarFunixprodComponent } from './components/sidebar/sidebar-funixprod/sidebar-funixprod.component';
-import { TopbarComponent } from './components/topbar/topbar.component';
-import { AlertsComponent } from './components/topbar/alerts/alerts.component';
-import { AlertContainerComponent } from './components/topbar/alerts/alert-container/alert-container.component';
-import { UserComponent } from './components/topbar/user/user.component';
+import {SidebarModule} from "./components/sidebar/sidebar.module";
+import {TopbarModule} from "./components/topbar/topbar.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent,
-    SidebarPacifistaComponent,
-    SidebarFunixprodComponent,
-    TopbarComponent,
-    AlertsComponent,
-    AlertContainerComponent,
-    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SidebarModule,
+    TopbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
