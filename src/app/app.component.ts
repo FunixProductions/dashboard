01abd8e772162b504d44ffb39ee5fa4e54b4f613
Antoine PRONNIER (Funix) <antoine.pronnier@gmail.com>
2023-01-18 15:57:51 +0100

    auth checker done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable, Subject} from "rxjs";
+import {UserDTO, UserRole} from "./dto/funix-api/user/user-dto";
+import {UserAuthService} from "./services/funix-api/user/user-auth-service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private userAuthService: UserAuthService,
+              private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.canActivate();
+  }
+
+  canActivate(): void {
+    const user: Observable<UserDTO> = this.userAuthService.currentUser();
+
+    user.subscribe({
+      next: (user: UserDTO) => {
+        if (user.role === UserRole.USER) {
+          this.router.navigate(['login']);
+        }
+      },
+      error: () => {
+        this.router.navigate(['login']);
+      }
+    });
+  }
 }
