0a73866e549d3f86cc4d9abbb4a38a0e86a6471b
Antoine PRONNIER (Funix) <antoine.pronnier@gmail.com>
2023-02-11 15:55:42 +0100

    fix dockerfile pteroq

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/ash
 rm -rf /home/container/*
 
-cp -rf /home/nginx /home/container/
+cp -rf /home/nginx/* /home/container/
 
 echo "⟳ Starting Nginx..."
 /usr/sbin/nginx -c /home/container/nginx/nginx.conf -p /home/container/
