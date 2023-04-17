71d19dff3cae5e63315afcd943787ee2decfe25e
Antoine PRONNIER (Funix) <antoine.pronnier@gmail.com>
2023-04-17 17:16:15 +0200

    pwa worker enabled by default

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import {isDevMode, NgModule} from '@angular/core';
+import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
@@ -19,9 +19,7 @@ import {ServiceWorkerModule} from '@angular/service-worker';
     BrowserAnimationsModule,
     MatSnackBarModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: !isDevMode(),
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
+      enabled: true,
       registrationStrategy: 'registerWhenStable:30000'
     })
   ],
