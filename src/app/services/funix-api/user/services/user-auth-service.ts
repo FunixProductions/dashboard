dbfff46d9c905bf2e0b97b1a449d582c342fbf47
FunixG <antoine.pronnier@gmail.com>
2023-04-25 14:30:51 +0200

    clean code

--- src/app/services/funix-api/user/services/user-auth-service.ts
+++ src/app/services/funix-api/user/services/user-auth-service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
-import {Observable, of, tap} from "rxjs";
+import {Observable} from "rxjs";
 import {UserCreationDTO} from "../dtos/requests/user-creation-dto";
 import {UserDTO} from "../dtos/user-dto";
 import {environment} from "../../../../../environments/environment";
@@ -15,7 +15,7 @@ export class UserAuthService extends FunixprodHttpClient {
 
   url: string = environment.funixApiUrl + 'user/auth/';
 
-  private userDtoCache: UserDTO | null = null;
+  private userDtoCache: Observable<UserDTO> | null = null;
 
   constructor(protected httpClient: HttpClient) {
     super();
@@ -30,25 +30,16 @@ export class UserAuthService extends FunixprodHttpClient {
   login(request: UserLoginDTO, captchaCode: string): Observable<UserTokenDTO> {
     super.addCaptchaToHeader(captchaCode);
 
-    return this.httpClient.post<UserTokenDTO>(this.url + 'login', request, {headers: super.headers}).pipe(
-      tap((userTokenDto: UserTokenDTO) => {
-        if (userTokenDto.user) {
-          this.userDtoCache = userTokenDto.user;
-        }
-      })
-    );
+    return this.httpClient.post<UserTokenDTO>(this.url + 'login', request, {headers: super.headers});
   }
 
   currentUser(): Observable<UserDTO> {
     if (this.userDtoCache !== null) {
-      return of(this.userDtoCache);
+      return this.userDtoCache;
+    } else {
+      this.userDtoCache = this.httpClient.get<UserDTO>(this.url + 'current', {headers: this.headers});
+      return this.userDtoCache;
     }
-
-    return this.httpClient.get<UserDTO>(this.url + 'current', {headers: super.headers}).pipe(
-      tap((userDto: UserDTO) => {
-        this.userDtoCache = userDto;
-      })
-    );
   }
 
 }
