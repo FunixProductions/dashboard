c18b331faa551edfae35b6e261b794bef905cb85
FunixG <antoine.pronnier@gmail.com>
2023-05-03 10:17:51 +0200

    Update notification when new version is out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,23 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SwUpdate} from "@angular/service-worker";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private swUpdate: SwUpdate) {
+  }
+
+  ngOnInit() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.versionUpdates.subscribe(() => {
+        if(confirm("Une nouvelle version du dashboard est disponible. Voulez-vous recharger la page ?")) {
+          window.location.reload();
+        }
+      });
+    }
+  }
 }
