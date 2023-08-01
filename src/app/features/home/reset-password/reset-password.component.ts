b6205c05fe3482f22395c3eec23e5004648b0cb2
FunixG <antoine.pronnier@gmail.com>
2023-08-01 16:07:01 +0200

    done forgotten password

--- src/app/features/home/reset-password/reset-password.component.ts
+++ src/app/features/home/reset-password/reset-password.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {UserAuthService} from "../../../services/funixproductions-api/user/services/user-auth-service";
 import UserPasswordResetDTO from "../../../services/funixproductions-api/user/dtos/requests/user-password-reset-dto";
 import NotificationsService from "../../../services/core/services/NotificationsService";
@@ -18,6 +18,7 @@ export class ResetPasswordComponent {
   emailReset: string = "";
 
   constructor(activeRoute: ActivatedRoute,
+              private router: Router,
               private userAuthService: UserAuthService,
               private captchaService: ReCaptchaV3Service,
               private notificationService: NotificationsService) {
@@ -26,7 +27,27 @@ export class ResetPasswordComponent {
     });
   }
 
-  sendResetPasswordRequest() {
+  resetPassword(): void {
+    if (this.passwordReset.newPassword && this.passwordReset.newPasswordConfirmation && this.passwordReset.newPassword.length > 0 && this.passwordReset.newPasswordConfirmation.length > 0) {
+      this.captchaService.execute("resetPassword").subscribe({
+        next: captchaCode => {
+          this.userAuthService.resetPassword(this.passwordReset, captchaCode).subscribe({
+            next: () => {
+              this.notificationService.success("Votre mot de passe a été réinitialisé.");
+              this.router.navigate(['/login']);
+            },
+            error: (error: HttpErrorResponse) => {
+              this.notificationService.onErrorRequest(error);
+            }
+          })
+        }
+      })
+    } else {
+      this.notificationService.warning("Veuillez entrer votre mot de passe.");
+    }
+  }
+
+  sendResetPasswordRequest(): void {
     if (this.emailReset.length > 0) {
       this.captchaService.execute("resetPasswordRequest").subscribe({
         next: captchaCode => {
